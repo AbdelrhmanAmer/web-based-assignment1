d3f508460effa9eea24318c0392911de787e5a7b
adel-3 <aboadel1512003@gmail.com>
2024-04-18 17:57:49 +0200

    front-end
    adding the form and the client-side validation

--- index.php
+++ index.php
@@ -1,3 +1,83 @@
-<?php
-	echo "Main registration page including header and footer.";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>regstration form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <?php
+        if (isset($_POST["submit"])) {
+            $fullname = $_POST["fullname"];
+            $username = $_POST["username"];
+            $birth_date = $_POST["birth-date"];
+            $phone = $_POST["phone"];
+            $address = $_POST["address"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $repeated_password = $_POST["repeated-password"];
+            $number    = preg_match('@[0-9]@', $password);
+            $specialChars = preg_match('@[^\w]@', $password);
+            $errors = array();
+
+
+            if (empty($fullname) or empty($username) or empty($birth_date) or empty($phone) or empty($address) or empty($email) or empty($password) or empty($repeated_password)) {
+                array_push($errors, "all fields are required");
+            }
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                array_push($errors, "the email is not valid");
+            }
+            if (strlen($password) < 8 or !$number or !$specialChars) {
+                array_push($errors, "Password should be at least 8 characters in length and include one number, and one special character");
+            }
+            if ($password !== $repeated_password) {
+                array_push($errors, "the password does not match");
+            }
+
+            if (count($errors) > 0) {
+                foreach ($errors as $error) {
+                    echo "<div class='alert alert-danger'>$error</div>";
+                }
+            }
+        }
+        ?>
+        <form action="index.php" method="post">
+            <div class="form-element">
+                <input type="text" class="form-control" name="fullname" placeholder="Full Name:">
+            </div>
+            <div class="form-element">
+                <input type="text" class="form-control" name="username" placeholder="User Name:">
+            </div>
+            <div class="form-element">
+                <input type="date" class="form-control" name="birth-date" placeholder="Birth Date:">
+            </div>
+            <div class="form-element">
+                <input type="number" class="form-control" name="phone" placeholder="Phone:">
+            </div>
+            <div class="form-element">
+                <input type="text" class="form-control" name="address" placeholder="Address:">
+            </div>
+            <div class="form-element">
+                <input type="email" class="form-control" name="email" placeholder="Email:">
+            </div>
+            <div class="form-element">
+                <input type="password" class="form-control" name="password" placeholder="Password:">
+            </div>
+            <div class="form-element">
+                <input type="password" class="form-control" name="repeated-password" placeholder="Repeated Password:">
+            </div>
+
+            <div class="form-element">
+                <input type="submit" class="btn-primarly" value="register" name="submit">
+            </div>
+        </form>
+    </div>
+
+</body>
+
+</html>
