7ca6527e40dc9b33b9e8ac7ec42fc8784a7603c8
Moreyad <91405739+reyad-7@users.noreply.github.com>
2024-04-22 12:50:53 +0200

    header & footer done
     Html structure and Css style also added

--- footer.php
+++ footer.php
@@ -1,3 +1,14 @@
-<?php
-    echo "Reusable footer component";
-?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="footerStyle.css">
+
+    <div class="footer">
+        <div class="footer-content">
+            <div class="foot-info"> &copy; 2024 <span> FCAI </span> All Rights Reserved</div>
+            <div class="social-icons">
+                <a href="#" target="_blank"><i class="fab fa-facebook"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-github"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-linkedin"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+    </div>
--- header.php
+++ header.php
@@ -1,3 +1,9 @@
-<?php 
-    echo "Reusable header component.";
-?>
+    <link rel="stylesheet" href="headerStyle.css">
+    <header class="header">
+        <div class="logo"> <a href="#">Registeration page</a></div>
+        <div class="nav">
+            <a href="#">Home</a>
+            <a href="#">Services</a>
+            <a href="#">Contact US</a>
+        </div>
+    </header>
